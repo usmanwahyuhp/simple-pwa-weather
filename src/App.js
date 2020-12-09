4a5691a16c64961c876ced029e4bce22fca10d77
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-10 04:49:25 +0700

    Done with the App

--- src/App.js
+++ src/App.js
@@ -32,6 +32,14 @@ const App = () => {
                         <span>{weather.name}</span>
                         <sup>{weather.sys.country}</sup>
                     </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
                 </div>
             )}
         </div>
