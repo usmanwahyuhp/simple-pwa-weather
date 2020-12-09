87170f7572472df4a3e09dc8ed62c15ca773ec16
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-10 04:39:57 +0700

    Implementing weather data to card

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css'
 
 const App = () => {
     const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
     
     const search = async(e) => {
         if(e.key === 'Enter') {
             const data = await fetchWeather(query)
 
-            console.log(data);
+            setWeather(data);
+            setQuery('');
         }
     }
 
@@ -24,6 +26,14 @@ const App = () => {
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyPress={search}
             />
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                </div>
+            )}
         </div>
     )
 }
